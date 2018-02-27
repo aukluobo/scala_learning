9db916cb92fc641e006c866dc4cd4e4e8802fcc0
aukluobo <aukluobo@gmail.com>
2018-02-27 16:38:03 +0800

    version 2
    correctly work to get result

--- printListSpiral.scala
+++ printListSpiral.scala
@@ -1,28 +1,47 @@
+import scala.collection.mutable.ListBuffer
+
 object listSpiral{
-    def main{
-        val rawList:List[List[Int]]=list(list(1,2,3,4),list(5,6,7,8),list(9,10,11,12),list(13,14,15,16))
+    def main(args:Array[String])={
+        val rawList:List[List[Int]]=List(List(1,2,3,4),List(5,6,7,8),List(9,10,11,12),List(13,14,15,16))
         val x:Int=0
         val y:Int=0
         val limit:Int=4
-        def loopList(tempRawData:List[List[Int]],tempX:Int,tempY:Int,tempLimit:Int):List[Int]={
-            val getList:List[Int] = for(i <- 0 to tempLimit){
-                    if(i>=tempY){
-                        val m:List[Int]=List.range(0,tempLimit)
-                        val t:List[Int]
-                        if(i>=(tempLimit+1)/2) t=m.reverse
-                        else t=m
-                        for(j <- t){
-                            if(j>=tempX && j<=(tempLimit-1-tempX)){
-                                val n=tempRawData(i)(j)
-                            }
+        def loopList(tempRawData:List[List[Int]],tempX:Int,tempY:Int,tempLimit:Int):Unit={
+            val getList = new ListBuffer[Int]
+            val headList = new ListBuffer[Int]
+            def makeLoop():Unit={
+                for(i <- 0 to tempLimit-1){
+                    if(i==tempY){
+                        for(p<-tempX to tempLimit-1-tempX){
+                            val sn=tempRawData(i){p}
+                            getList+=sn
                         }
                     }
-                } yield n
-            print getList mkString ","
+                    if(i==(tempLimit-1-tempY)){
+                        val m:List[Int] = List.range(tempX,tempLimit-tempX).reverse
+                        for(j<-m){
+                            val sn = tempRawData(i)(j)
+                            getList+=sn
+                        }
+                    }
+                    if(i>tempY && i<(tempLimit-1-tempY)){
+                        val h=tempRawData(i)(tempX)
+                        val n=tempRawData(i)(tempLimit-1-tempY)
+                        getList+=n
+                        headList+=h
+                    }
+                }
+            }
+            makeLoop
+            val gTemp:List[Int] = getList.toList
+            val hTemp:List[Int] = headList.toList.reverse
+            print( gTemp:::hTemp mkString ",")
+            print(",")
             val tempX1=tempX+1
             val tempY1=tempY+1
-            if(tempX1<=(tempLimit+1)/2) loopList(tempRawData,tempX1,tempY1,tempLimit)
+            if(tempX1<(tempLimit+1)/2) loopList(tempRawData,tempX1,tempY1,tempLimit)
         }
         loopList(rawList,x,y,limit)
+        println("\n")
     }
 }
